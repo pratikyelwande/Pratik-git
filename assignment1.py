1067d4687951ccb462197bd511fe2c5e253bdced
pratikyelwande <yelwandepratik11@gmail.com>
2022-04-29 07:46:05 +0530

    one file is commited

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+a=float(input("Enter the a: "))
+b=float(input("enter b: "))
+c=float(input("enter c: "))
+
+sum=a+b+c
+avg=sum/3
+#print("The sum is %f and the avg is %f"%(sum,avg))
+print("The sum is {} and avg is {}".format(sum,avg))
